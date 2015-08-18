65038feff0968b2aa8ed03cfd1000f56d7a53b56
Nigel Sampson <nigel.sampson@compiledexperience.com>
2015-08-18 22:09:58 +1200

    Forms + Windows

---
+++ Ignite-2015/Hubb.Forms.App.Windows/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Windows.ApplicationModel.Activation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Hubb.Forms.App.Windows.Views;
+
+namespace Hubb.Forms.App.Windows
+{
+    public sealed partial class App
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            var rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame == null)
+            {
+                rootFrame = new Frame
+                {
+                    Language = global::Windows.Globalization.ApplicationLanguages.Languages[0]
+                };
+
+
+                Xamarin.Forms.Forms.Init(e);
+
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                rootFrame.Navigate(typeof(MainView), e.Arguments);
+            }
+
+            Window.Current.Activate();
+        }
+    }
+}
---
+++ Ignite-2015/Hubb.Forms.App.Windows/Views/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Caliburn.Micro;
+using Hubb.Forms.Core;
+
+namespace Hubb.Forms.App.Windows.Views
+{
+    public sealed partial class MainView
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            
+            base.LoadApplication(new Application(IoC.Get<SimpleContainer>()));
+        }
+    }
+}
