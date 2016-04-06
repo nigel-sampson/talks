fd0a8699e828401e53d6845429e773f45c29eee7
Nigel Sampson <nigel.sampson@compiledexperience.com>
2016-04-07 00:01:22 +1200

    Unit tests

---
+++ mobile-services/Spending.Tests/Core/ViewModels/LoginViewModelTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.WindowsAzure.MobileServices;
+using Moq;
+using Spending.Core.Services;
+using Spending.Core.ViewModels;
+using Xunit;
+
+namespace Spending.Tests.Core.ViewModels
+{
+    public class LoginViewModelTests
+    {
+        private readonly Mock<IAuthenticationService> authentication;
+        private readonly Mock<IApplicationNavigationService> applicationNavigation;
+        private readonly LoginViewModel viewModel;
+
+        public LoginViewModelTests()
+        {
+            authentication = new Mock<IAuthenticationService>();
+            applicationNavigation = new Mock<IApplicationNavigationService>();
+
+            viewModel = new LoginViewModel(authentication.Object, applicationNavigation.Object);
+        }
+
+        [Fact]
+        public void LoginSuccessNavigatesCurrentExpenses()
+        {
+            authentication
+                .Setup(a => a.LoginAsync(MobileServiceAuthenticationProvider.MicrosoftAccount))
+                .ReturnsAsync(new MobileServiceUser("42"));
+
+            viewModel.LoginWithMicrosoftAccount();
+
+            applicationNavigation.Verify(n => n.ToCurrentExpenses());
+        }
+    }
+}
