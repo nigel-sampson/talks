640f5f0c6bd01ffe526b27f0bcf1fc55adf6b4ef
Nigel Sampson <nigel.sampson@compiledexperience.com>
2016-04-06 21:24:04 +1200

    Server notifications

--- mobile-services/Spending.App.Web/Controllers/ExpenseItemController.cs
+++ mobile-services/Spending.App.Web/Controllers/ExpenseItemController.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Web;
 using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.OData;
 using Microsoft.Azure.Mobile.Server;
+using Microsoft.Azure.Mobile.Server.Config;
+using Microsoft.Azure.NotificationHubs;
 using Spending.App.Web.DataObjects;
 using Spending.App.Web.Models;
 
@@ -41,12 +41,39 @@ namespace Spending.App.Web.Controllers
         {
             var current = await InsertAsync(item);
 
+            if (item.Amount > 100.0m)
+                await SendBigSpenderNotificationAsync();
+
             return CreatedAtRoute("Tables", new { id = current.Id }, current);
         }
 
         public Task DeleteExpenseItem(string id)
         {
             return DeleteAsync(id);
+        }
+
+
+        private async Task SendBigSpenderNotificationAsync()
+        {
+            var settings = Configuration.GetMobileAppSettingsProvider().GetMobileAppSettings();
+
+            var hubName = settings.NotificationHubName;
+            var hubConnection = settings.Connections[MobileAppSettingsKeys.NotificationHubConnectionString].ConnectionString;
+
+            var hub = NotificationHubClient.CreateClientFromConnectionString(hubConnection, hubName);
+
+            var windowsToastPayload = @"<toast><visual><binding template=""ToastText01""><text id=""1"">We've got a big spender here!</text></binding></visual></toast>";
+
+            try
+            {
+                var result = await hub.SendWindowsNativeNotificationAsync(windowsToastPayload);
+
+                Configuration.Services.GetTraceWriter().Info(result.State.ToString());
+            }
+            catch (Exception ex)
+            {
+                Configuration.Services.GetTraceWriter().Error(ex.Message);
+            }
         }
     }
 }
