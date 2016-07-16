e88b070c89ecd553f12b574cf759798e1d0c9cba
Nigel Sampson <nigel.sampson@compiledexperience.com>
2016-07-16 12:14:08 +1200

    Create iOS project

---
+++ ndc-sydney/NDC.Build.App.iOS/AppDelegate.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Principal;
+using Foundation;
+using UIKit;
+
+namespace NDC.Build.App.iOS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : UIApplicationDelegate
+    {
+        public override UIWindow Window
+        {
+            get;
+            set;
+        }
+
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            Window = new UIWindow(UIScreen.MainScreen.Bounds);
+
+            var controller = new UIViewController
+            {
+                View = {BackgroundColor = UIColor.Blue}
+            };
+
+            Window.RootViewController = controller;
+            
+            Window.MakeKeyAndVisible();
+
+            return true;
+        }
+    }
+}
+
+
---
+++ ndc-sydney/NDC.Build.App.iOS/Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UIKit;
+
+namespace NDC.Build.App.iOS
+{
+    public class Application
+    {
+        public static void Main(string[] args)
+        {
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
