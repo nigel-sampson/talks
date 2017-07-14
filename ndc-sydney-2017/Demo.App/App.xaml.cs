23aa07efad41b1999da2b684f9c3677e348ff5d4
Nigel Sampson <nigel.sampson@compiledexperience.com>
2017-07-15 00:05:05 +1200

    Add protcol support

--- ndc-sydney-2017/Demo.App/App.xaml.cs
+++ ndc-sydney-2017/Demo.App/App.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Windows.ApplicationModel.Activation;
 using Windows.UI.Xaml.Controls;
 using Caliburn.Micro;
+using Demo.Core.Messages;
 using Demo.Core.ViewModels;
 using Octokit;
 
@@ -32,6 +34,33 @@ namespace Demo.App
         protected override void OnLaunched(LaunchActivatedEventArgs args)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override void OnActivated(IActivatedEventArgs args)
+        {
+            if (args.Kind == ActivationKind.Protocol)
+            {
+                var protocolArgs = (ProtocolActivatedEventArgs) args;
+                (var success, var owner, var name) = ParseProtocol(protocolArgs.Uri);
+
+                if (success)
+                {
+                    var eventAggregator = container.GetInstance<IEventAggregator>();
+
+                    eventAggregator.PublishOnCurrentThread(new RepositorySelectedMessage(owner, name));
+                }
+            }
+        }
+
+        private static (bool success, string owner, string name) ParseProtocol(Uri uri)
+        {
+            var expression = new Regex(@"ndc://(?<owner>[\w-]+)/(?<name>[\w-]+)");
+            var match = expression.Match(uri.AbsoluteUri);
+
+            if (!match.Success)
+                return (false, null, null);
+
+            return (true, match.Groups["owner"].Value, match.Groups["name"].Value);
         }
 
         protected override object GetInstance(Type service, string key)
